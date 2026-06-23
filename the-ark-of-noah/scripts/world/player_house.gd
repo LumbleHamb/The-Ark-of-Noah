d833234301771af01e15c89008c92141a586e17e
LumbleHamb <ethen@harrelsons.org>
2026-06-23 14:14:45 -0500

    animating, tileset, and cabin

---
+++ the-ark-of-noah/scripts/world/player_house.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var anim_player = $AnimationPlayer
+@onready var chimney_smoke: AnimatedSprite2D = $chimney_smoke
+
+
+func _on_doorway_body_entered(body: Node2D) -> void:
+	print("ENTER:", body.name)
+
+	if body.name == "player":
+		chimney_smoke.visible = false
+		anim_player.play("open_house")
+
+
+func _on_doorway_body_exited(body: Node2D) -> void:
+	print("EXIT:", body.name)
+
+	if body.name == "player":
+		chimney_smoke.visible = true
+		anim_player.play_backwards("open_house")
